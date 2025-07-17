03731ed050c4857f3eb2f8008636495a82f0d4c0
Wasif Saeed(2116) <wasif.prosku@nextbridge.com>
2025-07-17 12:21:09 +0500

    Update .gitignore and clean up ignored files

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,4 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = 'blog-home'
